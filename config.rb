9daa782fc25f0444c3aa82ba0fa4756d0cfd8568
kara <kdiaby.pro@gmail.com>
2017-05-08 13:03:34 +0200

    flat dynamic view finished

--- config.rb
+++ config.rb
@@ -27,10 +27,9 @@ activate :autoprefixer
 #   page "/admin/*"
 # end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
+["kara", "charles", "christian"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
 ###
 # Helpers
 ###
